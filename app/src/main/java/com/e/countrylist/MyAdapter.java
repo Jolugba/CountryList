cfa36dea529d57a9296013c4cdcb09ae91948cbe
missTinuade <estherjolugba@yahoo.com>
2020-03-08 14:34:24 +0000

    fix svg error

--- app/src/main/java/com/e/countrylist/MyAdapter.java
+++ app/src/main/java/com/e/countrylist/MyAdapter.java
@@ -1,6 +1,8 @@
 package com.e.countrylist;
 
 import android.content.Context;
+import android.graphics.drawable.PictureDrawable;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,15 +12,20 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.ahmadrosid.svgloader.SvgLoader;
+import com.bumptech.glide.Glide;
+import com.caverock.androidsvg.SVG;
 import com.squareup.picasso.OkHttp3Downloader;
 import com.squareup.picasso.Picasso;
 
+import java.io.InputStream;
 import java.util.List;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewholder> {
     Context context;
     LayoutInflater inflater;
     List<Model> list;
+     MainActivity activity;
 
     public MyAdapter(Context context, List<Model> list) {
         this.context = context;
@@ -49,12 +56,11 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewholder> {
     public void onBindViewHolder(@NonNull MyAdapter.MyViewholder holder, int position) {
             holder.textView.setText(list.get(position).getName());
             holder.textView2.setText(list.get(position).capital);
-        Picasso.Builder builder= new Picasso.Builder(context);
-        builder.downloader(new OkHttp3Downloader(context));
-        builder.build().load(list.get(position).getFlag())
-                .error(R.drawable.ic_launcher_background)
-                .into(holder.imageView);
-
+            Glide.with(context)
+                    .as(PictureDrawable::class.java)
+                    .listener(new SvgSoftwareLayerSetter())
+                    .load(list.get(position).getFlag())
+                    .into(imageView);
     }
 
     @Override
